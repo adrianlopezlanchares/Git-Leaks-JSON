b927aa5c92044846a633110ffe5abbd7d292faad
adrianlopezlanchares <202107370@alu.comillas.edu>
2022-11-16 10:31:34 +0100

    Cambios para que funcione bien

--- gitLeaksJSON.py
+++ gitLeaksJSON.py
@@ -1,27 +1,23 @@
 from git import Repo
-import re 
-import sys
-import signal
-import pprint
-
-# Variables globales:
-# Ruta del repositorio en el que se van a buscar commits
-REPO_DIR = './skale-manager'
-
-# Lista de palabras clave a buscar en los commits
-KEY_WORDS = ['credentials', 'password', 'key', 'username', 'private']
+import re, sys, signal, pprint
 
 
-def extraer(repo_dir):
+
+DIR_REPO = './skale-manager'    # Ruta del repositorio en el que se va a buscar
+KEY_WORDS = ['credentials', 'password', 'key', 'username', 'private']   # Lista de palabras clave a buscar en los commits
+
+
+def extraer(dir_repo):
     #Extrae los commits del repositorio
-    repo = Repo(repo_dir)
+
+    repo = Repo(dir_repo)
     commits = list(repo.iter_commits('develop'))
     return commits
 
 def tranformar(commits):
     # Transforma los commits que nos interesan en un diccionario
-    diccionario = dict()
-    
+
+    diccionario = {}
     for commit in commits:
         for word in KEY_WORDS:
             if re.search(word, commit.message, re.IGNORECASE):
@@ -30,7 +26,6 @@ def tranformar(commits):
                 diccionario[commit.hexsha] =  commit.message
     return diccionario
 
-
 def cargar(datos):
     # Carga los datos. En este caso, los cargamos en un archivo json
     with open('datos.json', 'w') as f:
@@ -38,25 +33,11 @@ def cargar(datos):
 
 
 
-def handler_signal(signal, frame):
-    '''
-    Función que maneja la señal SIGINT (CTRL + C)
-    '''
-    # Imprime un mensaje y sale del programa
-    print("\n\n[!] Out ............. \n")
-    sys.exit(1)
-
-
+def main():
+    commits = extraer(DIR_REPO) # Extrae los commits
+    datos = tranformar(commits) # Transforma los commits en un diccionario
+    
+    cargar(datos)   # CArgamos los datos en un archivo JSON
 
 if __name__ == '__main__':
-    # Para que el main pueda registrar la señal SIGINT
-    signal.signal(signal.SIGINT, handler_signal)
-
-    # Extrae los commits
-    commits = extraer(REPO_DIR)
-
-    # Los transforma para imprimirlos
-    datos = tranformar(commits)
-    
-    # Los imprime
-    cargar(datos)
+    main()
