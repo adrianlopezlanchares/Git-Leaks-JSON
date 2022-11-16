6215872c43345107b9d2055d8a5341649641d1c0
adrianlopezlanchares <202107370@alu.comillas.edu>
2022-11-16 11:26:19 +0100

    Bloque 3 Practica 1

--- gitLeaksJSON.py
+++ gitLeaksJSON.py
@@ -1,43 +1,35 @@
 from git import Repo
-import re, sys, signal, pprint
+import re, pprint
 
-
-
-DIR_REPO = './skale-manager'    # Ruta del repositorio en el que se va a buscar
-KEY_WORDS = ['credentials', 'password', 'key', 'username', 'private']   # Lista de palabras clave a buscar en los commits
-
+dir_repo = "./directorio_repo"
+key_words = ["credentials", "password", "key", "username", "private"]
 
 def extraer(dir_repo):
-    #Extrae los commits del repositorio
-
+    # Extraemos todos los commits del repositorio
     repo = Repo(dir_repo)
     commits = list(repo.iter_commits('develop'))
     return commits
 
-def tranformar(commits):
-    # Transforma los commits que nos interesan en un diccionario
-
-    diccionario = {}
+def transformar(commits):
+    # Transformamos los datos, y guardamos los commits que nos interesan en un diccionario
+    dict_commits = {}
     for commit in commits:
-        for word in KEY_WORDS:
+        for word in key_words:
             if re.search(word, commit.message, re.IGNORECASE):
-                # Si la encuentra, añade al diccionario el "id"(versión en la que se hace) 
-                # del commit y el mensaje que se hubiera introducido
-                diccionario[commit.hexsha] =  commit.message
-    return diccionario
+                # Guardamos en el diccionario el id del commit y el mensaje que se hubiera introducido
+                dict_commits[commit.hexsha] = commit.message
+    return dict_commits
 
 def cargar(datos):
-    # Carga los datos. En este caso, los cargamos en un archivo json
+    # Cargamos los datos. En este caso, los escribimos en un fichero JSON
     with open('datos.json', 'w') as f:
         pprint.pprint(datos, stream=f)
 
-
-
 def main():
-    commits = extraer(DIR_REPO) # Extrae los commits
-    datos = tranformar(commits) # Transforma los commits en un diccionario
-    
-    cargar(datos)   # CArgamos los datos en un archivo JSON
+    commits = extraer(dir_repo)
+    datos = transformar(commits)
+    cargar(datos)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
